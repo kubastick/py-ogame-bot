c4fe28285e887730ac3653c4fd4cf650599ed17d
Jakub Tomana <jakub.tomana@protonmail.com>
2019-01-02 21:36:44 +0100

    Reformat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from selenium import webdriver
-
 from ogame_controller import OgameController
 
 print("Loading ogame bot")
 ogame_controller = OgameController("C:\\bin\\chromedriver.exe")
 print("Logging in")
-ogame_controller.login("admin","1234")
+ogame_controller.login("admin", "1234")
